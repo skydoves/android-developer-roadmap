0c24210719e9979bd2cf9e1813879c0ff6f9387c
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2021-12-02 22:29:33 +0530

    Fix Crash for low end devices. Used Coil instead of Glide to load Image.

--- app/src/main/kotlin/io/getstream/androiddeveloperroadmap/MainActivity.kt
+++ app/src/main/kotlin/io/getstream/androiddeveloperroadmap/MainActivity.kt
@@ -19,53 +19,55 @@ package io.getstream.androiddeveloperroadmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.mxalbert.zoomable.Zoomable
-import com.skydoves.landscapist.glide.GlideImage
 import io.getstream.androiddeveloperroadmap.ui.theme.AndroidDeveloperRoadmapTheme
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    setContent {
-      AndroidDeveloperRoadmapTheme {
-        Surface(
-          color = MaterialTheme.colors.background
-        ) {
-          Roadmap()
+        setContent {
+            AndroidDeveloperRoadmapTheme {
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    Roadmap()
+                }
+            }
         }
-      }
     }
-  }
 }
 
 @Composable
 private fun Roadmap() {
-  Zoomable(
-    modifier = Modifier.height(3200.dp)
-  ) {
-    GlideImage(
-      imageModel = "https://user-images.githubusercontent.com/24237865/144350753-5a52e6e5-3517-476c-8e5c-adad919abe8e.png",
-      modifier = Modifier.fillMaxSize(),
-      previewPlaceholder = R.drawable.roadmap_preview,
-      contentScale = ContentScale.Fit
-    )
-  }
+    Zoomable(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Image(
+            painter = rememberImagePainter("https://user-images.githubusercontent.com/24237865/144350753-5a52e6e5-3517-476c-8e5c-adad919abe8e.png",
+                builder = {
+                    placeholder(R.drawable.roadmap_preview)
+                }),
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Fit,
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun DefaultPreview() {
-  AndroidDeveloperRoadmapTheme {
-    Roadmap()
-  }
+    AndroidDeveloperRoadmapTheme {
+        Roadmap()
+    }
 }
