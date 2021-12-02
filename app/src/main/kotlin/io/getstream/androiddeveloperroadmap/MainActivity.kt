169d29410a4e998b5b8dcc1d90c724bb2f44b7af
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2021-12-02 22:43:39 +0530

    Formatting improved

--- app/src/main/kotlin/io/getstream/androiddeveloperroadmap/MainActivity.kt
+++ app/src/main/kotlin/io/getstream/androiddeveloperroadmap/MainActivity.kt
@@ -32,42 +32,44 @@ import com.mxalbert.zoomable.Zoomable
 import io.getstream.androiddeveloperroadmap.ui.theme.AndroidDeveloperRoadmapTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        setContent {
-            AndroidDeveloperRoadmapTheme {
-                Surface(
-                    color = MaterialTheme.colors.background
-                ) {
-                    Roadmap()
-                }
-            }
+    setContent {
+      AndroidDeveloperRoadmapTheme {
+        Surface(
+          color = MaterialTheme.colors.background
+        ) {
+          Roadmap()
         }
+      }
     }
+  }
 }
 
 @Composable
 private fun Roadmap() {
-    Zoomable(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Image(
-            painter = rememberImagePainter("https://user-images.githubusercontent.com/24237865/144350753-5a52e6e5-3517-476c-8e5c-adad919abe8e.png",
-                builder = {
-                    placeholder(R.drawable.roadmap_preview)
-                }),
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Fit,
-            contentDescription = null
-        )
-    }
+  Zoomable(
+    modifier = Modifier.fillMaxSize(),
+  ) {
+    Image(
+      painter = rememberImagePainter(
+        "https://user-images.githubusercontent.com/24237865/144350753-5a52e6e5-3517-476c-8e5c-adad919abe8e.png",
+        builder = {
+          placeholder(R.drawable.roadmap_preview)
+        }
+      ),
+      modifier = Modifier.fillMaxSize(),
+      contentScale = ContentScale.Fit,
+      contentDescription = null
+    )
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun DefaultPreview() {
-    AndroidDeveloperRoadmapTheme {
-        Roadmap()
-    }
+  AndroidDeveloperRoadmapTheme {
+    Roadmap()
+  }
 }
